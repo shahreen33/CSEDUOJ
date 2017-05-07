17e96a0070334c621a31cb98b45a384af1491ecd
Tanzir Islam <Tanzir5@Tanzirs-MacBook-Air.local>
2017-05-07 13:42:49 +0600

    SpojBot Ready

--- index.php
+++ index.php
@@ -15,8 +15,8 @@ and open the template in the editor.
         $item1='QTREE';
         $item2='code.txt';
         $item3='6';
-        //$tmp = exec("python login-submit.py");
-        $tmp = exec("python spojBot.py ".$item1." ".$item2." ".$item3);
+        $tmp = exec("python simple.py");
+        //$tmp = exec("python spojBot.py ".$item1." ".$item2." ".$item3);
         
         echo $tmp;
 
--- simple.py
+++ simple.py
@@ -1,7 +1,3 @@
-import sys
 import time
-import numpy
 time.sleep(1)
-print numpy.abs(-23)
-print sys.argv[1] + sys.argv[2] + sys.argv[3]
-#print "yo yo whatever"
+print "yo yo whatever"
