f9bae1ce84bc9d67db355f677e72f2136c0392cf
Tanzir Islam <Tanzir5@Tanzirs-MacBook-Air.local>
2017-05-07 13:46:18 +0600

    SpojBot Ready

--- simple.py
+++ simple.py
@@ -1,3 +1,10 @@
 import time
+import os
+import requests
+import re
+import sys
+from bs4 import BeautifulSoup
 time.sleep(1)
-print "yo yo whatever"
+html = 'df'
+soup = BeautifulSoup(html, 'html.parser')
+print soup
